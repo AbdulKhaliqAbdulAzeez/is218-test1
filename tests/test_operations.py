6d0aa51b568b42c1ead61998f529e98e4329533f
AbdulKhaliqAbdulAzeez <abdulkhailqabdulazeez@gmail.com>
2025-09-30 15:26:40 -0400

    feature: implement multiply() with unit tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,6 @@
 import pytest 
 
-from src.operations import add, subtract
+from src.operations import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
@@ -11,3 +11,8 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
+    
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
